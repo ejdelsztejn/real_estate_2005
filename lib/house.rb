c554cd578a9fe7f35b7bde33ed9e48d23368a1fa
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-13 11:18:09 -0400

    :sparkles: Add rooms
    Add rooms

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,8 @@
 class House
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
   def initialize(price, address)
     @price = price.delete('$').to_i
     @address = address
+    @rooms = []
   end
 end
