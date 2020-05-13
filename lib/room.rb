9fa665fecd8bcc0b5bc46add8984aa043cae15ae
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-13 11:07:07 -0400

    :sparkles: Add is_painted? method
    Add is_painted? method

--- lib/room.rb
+++ lib/room.rb
@@ -1,15 +1,21 @@
 class Room
   attr_reader :category,
               :length,
-              :width
+              :width,
+              :is_painted
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width.to_i
+    @is_painted = false
   end
 
   def area
     @length * @width
+  end
+
+  def is_painted?
+    @is_painted
   end
 end
