ba65b0f815ea142672fec3612c1780f61b433f8f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-13 11:20:23 -0400

    :sparkles: Add test_it_can_add_rooms
    Add test_it_can_add_rooms

--- test/house_test.rb
+++ test/house_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/room'
 require './lib/house'
 
 class HouseTest < Minitest::Test
@@ -25,5 +26,19 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
 
     assert_equal [], house.rooms
+  end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal [], house.rooms
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
   end
 end
