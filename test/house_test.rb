223ba6f9b5c22073b665b39c54c20a73526d77d5
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-13 12:00:03 -0400

    :sparkles: Add test_it_can_sort_rooms_by_category
    Add test_it_can_sort_rooms_by_category

--- test/house_test.rb
+++ test/house_test.rb
@@ -115,4 +115,21 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
   end
+
+  def test_it_can_sort_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms_by_category[:bedroom]
+    assert_equal [room_3], house.rooms_by_category[:living_room]
+    assert_equal [room_4], house.rooms_by_category[:basement]
+  end
 end
