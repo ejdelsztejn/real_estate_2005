01bf8ddfdab7a96d90257c48e6ab6a25fec26d7b
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-13 12:50:56 -0400

    :white_check_mark: Add additional test cases
    Add additional test cases

--- test/house_test.rb
+++ test/house_test.rb
@@ -14,12 +14,20 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
 
     assert_equal 400000, house.price
+
+    house2 = House.new("$1000000", "45 sugar lane")
+
+    assert_equal 1000000, house2.price
   end
 
   def test_it_has_an_address
     house = House.new("$400000", "123 sugar lane")
 
     assert_equal "123 sugar lane", house.address
+
+    house2 = House.new("$1000000", "45 sugar lane")
+
+    assert_equal "45 sugar lane", house2.address
   end
 
   def test_it_starts_without_rooms
@@ -46,6 +54,10 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
 
     assert_equal false, house.above_market_average?
+
+    house2 = House.new("$1000000", "45 sugar lane")
+
+    assert_equal true, house2.above_market_average?
   end
 
   def test_it_can_return_number_of_rooms_by_category
@@ -77,6 +89,21 @@ class HouseTest < Minitest::Test
     house.add_room(room_4)
 
     assert_equal 1900, house.area
+
+    house2 = House.new("$1000000", "45 sugar lane")
+    room_5 = Room.new(:kitchen, 20, '45')
+    room_6 = Room.new(:bedroom, 15, '20')
+    room_7 = Room.new(:living_room, 30, '20')
+    room_8 = Room.new(:basement, 40, '62')
+    room_9 = Room.new(:bedroom, 10, '15')
+
+    house2.add_room(room_5)
+    house2.add_room(room_6)
+    house2.add_room(room_7)
+    house2.add_room(room_8)
+    house2.add_room(room_9)
+
+    assert_equal 4430, house2.area
   end
 
   def test_it_can_return_details
@@ -99,6 +126,21 @@ class HouseTest < Minitest::Test
     house.add_room(room_2)
 
     assert_equal 210.53, house.price_per_square_foot
+
+    house2 = House.new("$1000000", "45 sugar lane")
+    room_5 = Room.new(:kitchen, 20, '45')
+    room_6 = Room.new(:bedroom, 15, '20')
+    room_7 = Room.new(:living_room, 30, '20')
+    room_8 = Room.new(:basement, 40, '62')
+    room_9 = Room.new(:bedroom, 10, '15')
+
+    house2.add_room(room_5)
+    house2.add_room(room_6)
+    house2.add_room(room_7)
+    house2.add_room(room_8)
+    house2.add_room(room_9)
+
+    assert_equal 225.73, house2.price_per_square_foot
   end
 
   def test_it_can_sort_rooms_by_area
@@ -114,6 +156,21 @@ class HouseTest < Minitest::Test
     house.add_room(room_2)
 
     assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
+
+    house2 = House.new("$1000000", "45 sugar lane")
+    room_5 = Room.new(:kitchen, 20, '45')
+    room_6 = Room.new(:bedroom, 15, '20')
+    room_7 = Room.new(:living_room, 30, '20')
+    room_8 = Room.new(:basement, 40, '62')
+    room_9 = Room.new(:bedroom, 10, '15')
+
+    house2.add_room(room_5)
+    house2.add_room(room_6)
+    house2.add_room(room_7)
+    house2.add_room(room_8)
+    house2.add_room(room_9)
+
+    assert_equal [room_8, room_5, room_7, room_6, room_9], house2.rooms_sorted_by_area
   end
 
   def test_it_can_sort_rooms_by_category
